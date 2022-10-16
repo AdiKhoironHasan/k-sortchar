481c0492dde560cdc31a98afd499828a128fb32d
AdiKhoironHasan <adieron97@gmail.com>
2022-10-16 14:31:05 +0700

    done 3

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -22,65 +23,127 @@ func sortChar(text string) (string, string) {
 	text = strings.ToLower(text)
 	text = strings.ReplaceAll(text, " ", "")
 
-	vowelsMap := make(map[string]int)
-	consonantMap := make(map[string]int)
+	type data struct {
+		Key   string
+		Sort  int
+		Value int
+	}
 
+	vowelsMap := make(map[string]data)
+	consonantMap := make(map[string]data)
+
+	sortVow := 1
+	sortCon := 1
 	for _, val := range text {
 		switch val {
 		case 'a':
 			if val, ok := vowelsMap["a"]; !ok {
-				vowelsMap["a"] = 1
+				vowelsMap["a"] = data{
+					Sort:  sortVow,
+					Value: 1,
+				}
 			} else {
-				vowelsMap["a"] = val + 1
+				vowelsMap["a"] = data{
+					Sort:  sortVow,
+					Value: val.Value + 1,
+				}
 			}
+
+			sortVow++
 		case 'e':
 			if val, ok := vowelsMap["e"]; !ok {
-				vowelsMap["e"] = 1
+				vowelsMap["e"] = data{
+					Sort:  sortVow,
+					Value: 1,
+				}
 			} else {
-				vowelsMap["e"] = val + 1
+				vowelsMap["e"] = data{
+					Sort:  sortVow,
+					Value: val.Value + 1,
+				}
 			}
 		case 'i':
 			if val, ok := vowelsMap["i"]; !ok {
-				vowelsMap["i"] = 1
+				vowelsMap["i"] = data{
+					Sort:  sortVow,
+					Value: 1,
+				}
 			} else {
-				vowelsMap["i"] = val + 1
+				vowelsMap["i"] = data{
+					Sort:  sortVow,
+					Value: val.Value + 1,
+				}
 			}
 		case 'o':
 			if val, ok := vowelsMap["o"]; !ok {
-				vowelsMap["o"] = 1
+				vowelsMap["o"] = data{
+					Sort:  sortVow,
+					Value: 1,
+				}
 			} else {
-				vowelsMap["o"] = val + 1
+				vowelsMap["o"] = data{
+					Sort:  sortVow,
+					Value: val.Value + 1,
+				}
 			}
 		case 'u':
 			if val, ok := vowelsMap["u"]; !ok {
-				vowelsMap["u"] = 1
+				vowelsMap["u"] = data{
+					Sort:  sortVow,
+					Value: 1,
+				}
 			} else {
-				vowelsMap["u"] = val + 1
+				vowelsMap["u"] = data{
+					Sort:  sortVow,
+					Value: val.Value + 1,
+				}
 			}
 
 		default:
 			v := fmt.Sprint(string(val))
 			if val, ok := consonantMap[v]; !ok {
-				consonantMap[v] = 1
+				consonantMap[v] = data{
+					Sort:  int(sortCon),
+					Value: 1,
+				}
+				sortCon++
 			} else {
-				consonantMap[v] = val + 1
+				consonantMap[v] = data{
+					Sort:  val.Sort,
+					Value: val.Value + 1,
+				}
 			}
 		}
 	}
 
+	var sortedConsonant []data
+
+	for k, v := range consonantMap {
+		sortedConsonant = append(sortedConsonant, data{
+			Key:   k,
+			Value: v.Value,
+			Sort:  v.Sort,
+		})
+	}
+
+	sort.Slice(sortedConsonant, func(i, j int) bool {
+		return sortedConsonant[i].Sort < sortedConsonant[j].Sort
+	})
+
 	// fmt.Println(vowelsMap)
-	// fmt.Println(consonantMap)
+	// fmt.Println(sortedConsonant)
 
 	var vowels string
 	var consonant string
 	for key, val := range vowelsMap {
-		for i := 0; i < val; i++ {
+		for i := 0; i < val.Value; i++ {
 			vowels = fmt.Sprintf(vowels + key)
 		}
 	}
-	for key, val := range consonantMap {
-		for i := 0; i < val; i++ {
-			consonant = fmt.Sprintf(consonant + key)
+
+	for _, val := range sortedConsonant {
+		for i := 0; i < val.Value; i++ {
+			consonant = fmt.Sprintf(consonant + val.Key)
 		}
 	}
 
